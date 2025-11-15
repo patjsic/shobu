623b3c7ac9d25ede2de7a0cfd910b60afe2786c5
Patrick Sicurello <43273424+patjsic@users.noreply.github.com>
2025-11-14 21:12:47 -0500

    fix diagonal movement

--- src/board.py
+++ src/board.py
@@ -61,29 +61,40 @@ class Quad:
         assert start[1] in range(4), f"{start} out of bounds"
         assert end[0] in range(4), f"{end} out of bounds"
         assert end[1] in range(4), f"{end} out of bounds"
+
         # Two cases: straight line and diagonal
-        diff = [end[1] - start[1], end[0] - start[0]]
+        diff = [abs(end[1] - start[1]), abs(end[0] - start[0])]
         if max(diff[0], diff[1]) <= 2:
             if diff[0] == 0 or diff[1] == 0: # check straight line
                 return True
-            elif sum(diff) <= 2: # check diagonal
+            elif (diff[1] / diff[0]) <= 1: # check diagonal
                 return True
             else:
                 return False
         else:
             return False
 
-    def move_piece(self, start: Tuple[int, int], end: Tuple[int, int], first_player: bool = True, passive: bool = True) -> None:
+    def move_piece(self, start: Tuple[int, int], end: Tuple[int, int], first_player: bool = True, passive: bool = True) -> bool:
         """
         Take start (row, col) position and move piece to end (row, col) position.
+
+        Returns True if piece was moved, false if not.
         """
-        if self.check_move(start, end):
+        piece = self.piece1 if first_player else self.piece2
+        if piece == self.nullpiece or self.get(start[0], start[1]) == self.nullpiece:
+            # Cannot move a piece that isn't there!
+            print("Tried to move air...")
+            return False
+        elif self.check_move(start, end):
             #TODO: Check moves only max 2 consecutive spaces
             #TODO: Check that pieces do not intersect or 'push' other pieces on passive move
             #TODO: Implement aggresive move
-            self.set(start[0], start[1], self.nullpiece, override=True) # we know this is always filled
-            self.set(end[0], end[1], self.piece1 if first_player else self.piece2)
+            self.set(start[0], start[1], self.nullpiece) # we know this is always filled
+            self.set(end[0], end[1], piece)
+            return True
         else:
+            # return False
+            # Bad moves raise an error to fortify rules to user.
             raise ValueError(f"Could not move piece from {start} to {end}. Please check if move is valid.")
 
 class Board:
@@ -96,10 +107,19 @@ if __name__=="__main__":
     test.reset_quad()
     print(test)
     test.move_piece((3, 0), (2, 1), True)
-
     print(test)
     # test.move_piece((0,0), (1, 2), False) # This is an invalid move!
-    test.move_piece((0, 0), (2, 0), False) # TODO: This case is failing....
+    test.move_piece((0, 0), (2, 0), False)
+    test.move_piece((0, 0), (2, 0), False)
+    test.move_piece((0, 0), (2, 0), False)
+    print(test)
+    test.move_piece((3, 1), (1, 3), True) #TODO: This fails but should be a valid move...
+    print(test)
+    # test.move_piece((1, 3), (3, 0), True)  # This should fail
+    test.move_piece((0, 0), (2, 0), False)
+    test.move_piece((0, 0), (5, 0), False) #This doesn't fail because there is no piece being moved.
+    print(test)
+    test.move_piece((3, 3), (0, 0), True) # This should fail since it moves diagonal > 2 spaces
     print(test)
     test.reset_quad()
     print(test)
